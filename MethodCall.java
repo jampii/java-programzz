1b544a86acb8253841c9ea7fbe70bc2c42f7401a
saikumar jampana <40858544+jampii@users.noreply.github.com>
2018-07-13 19:53:31 +0530

    Add files via upload

---
+++ MethodCall.java
@@ -0,0 +1,16 @@
+// ** Created a subclass and calling those methods to main class**//
+
+class Old {
+  public static void main (String args[]){
+		 New Newobj = new New();
+		 Newobj.MyText();
+	 }
+}
+
+//** sub class **//
+
+ class New {
+	 public  void MyText (){
+		 System.out.println("My name is SaiKumar");
+}
+}
